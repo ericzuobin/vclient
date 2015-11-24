4e9327c51f7800e73d300b4f56be35eaa827b384
sahinn <=>
2015-11-24 18:06:16 +0800

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to vclient
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/net/zuobin/vclient_test.clj
@@ -0,0 +1,7 @@
+(ns net.zuobin.vclient-test
+  (:require [clojure.test :refer :all]
+            [net.zuobin.vclient :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
